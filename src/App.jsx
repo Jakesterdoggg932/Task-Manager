f868bd9d7f4063d13bcb0db4c930bf6cb92e6d17
Jakesterdoggg932 <117101760+Jakesterdoggg932@users.noreply.github.com>
2026-05-19 01:37:34 -0700

    Auth test

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,17 @@ import LoginPage from './Components/auth/LoginPage';
 import RegisterPage from './Components/auth/RegisterPage';
 import AboutPage from './Components/Bookkeeping/AboutPage';
 import { Route, Routes } from 'react-router-dom';
+import { Authenticator } from '@aws-amplify/ui-react';
+import { Amplify } from 'aws-amplify';
+import outputs from '@/amplify_outputs.json';
+import '@aws-amplify/ui-react/styles.css';
 
+Amplify.configure(outputs);
 
 function App() {
   return (
+    <Authenticator loginMechanisms={['email']}>
+      {({ signOut, user }) => (
     <div >
       <SiteNav />
       <Routes>
@@ -22,7 +29,8 @@ function App() {
       </Routes>
       <SiteFooter />
     </div>
+      )}
+    </Authenticator>
   );
 }
-
 export default App;
